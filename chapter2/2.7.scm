3db64a75aa27e4b73e9de15295206180ece17420
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-14 18:53:24 +0800

    chapter 2 exercise 7 8 9

---
+++ chapter2/2.7.scm
@@ -0,0 +1,8 @@
+(define (make-interval a b)
+  (cons a b))
+
+(define (upper-bound z)
+  (cdr z))
+
+(define (lower-bound z)
+  (car z))
---
+++ chapter2/2.9.scm
@@ -0,0 +1,8 @@
+(define (interval-width interval)
+  (/ (- (upper-bound interval) (lower-bound interval)) 2))
+
+(define (width-accumulated? combiner interval1 interval2)
+  (let ((width1 (interval-width interval1))
+	(width2 (interval-width interval2))
+	(combined-width (interval-width (combiner interval1 interval2))))
+   (or (= combined-width (+ width1 width2)) (= combined-width (- width1 width2)))))
