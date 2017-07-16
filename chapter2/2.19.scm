0c10abb4d35b46e5cc2ee7e49df8a152d4a5180c
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-16 13:43:10 +0800

    chapter 2 exercise 19

---
+++ chapter2/2.19.scm
@@ -0,0 +1,24 @@
+(define us-coins (list 50 25 10 5 1))
+
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (no-more? coin-values)) 0)
+	(else
+	 (+ (cc amount
+		(except-first-denomination coin-values))
+	    (cc (- amount
+		   (first-denomination coin-values))
+		coin-values)))))
+
+(define (first-denomination items)
+  (car items))
+
+(define (except-first-denomination items)
+  (cdr items))
+
+(define (no-more? items)
+  (null? items))
+
+;; the arrangement of list parameter coin-values has no influence on the output of cc
