89f18137a14b8e55201a16616d3291357754af31
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-16 16:45:56 +0800

    chapter 2 exercise 25

---
+++ chapter2/2.25.scm
@@ -0,0 +1,9 @@
+;; (1 3 (5 7) 9)
+;;(car (cdr (car (cdr (cdr '(1 3 (5 7) 9))))))
+
+;; ((7))
+;; (car (car '((7))))
+
+;; (1 (2 (3 (4 (5 (6 7)))))))
+;; (cadr (cadr (cadr (cadr (cadr (cadr third))))))
+
