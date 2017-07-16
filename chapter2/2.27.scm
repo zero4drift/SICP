97ac8e5b27a14e1e9cd68b61ab571b33441ab405
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-16 17:44:25 +0800

    chapter 2 exercise 27

---
+++ chapter2/2.27.scm
@@ -0,0 +1,8 @@
+(define (deep-reverse items)
+  (define (iter items result)
+    (cond ((null? items) result)
+	  ((pair? (car items))
+	   (iter (cdr items)
+		 (cons (deep-reverse (car items)) result)))
+	  (else (iter (cdr items) (cons (car items) result)))))
+  (iter items '()))
