7a7be883ab1ae240288742798efff5eb23af1410
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-16 21:24:31 +0800

    chapter 2 exercise 30 competed

--- chapter2/2.30.scm
+++ chapter2/2.30.scm
@@ -1,6 +1,16 @@
+;; with map
+
 (define (square-tree tree)
   (map (lambda (sub-tree)
 	 (if (pair? sub-tree)
 	     (square-tree sub-tree)
 	     (square sub-tree)))
        tree))
+
+;; without map
+
+(define (square-tree-simple tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (square tree))
+	(else (cons (square-tree-simple (car tree))
+		    (square-tree-simple (cdr tree))))))
