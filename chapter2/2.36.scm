130a7cdc62ec298a7518a9e0c5ce3dcd5b4df796
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-17 17:03:28 +0800

    chapter 2 exercise 36

---
+++ chapter2/2.36.scm
@@ -0,0 +1,5 @@
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+	    (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
