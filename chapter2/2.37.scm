fd69bd84ff5743535bfd60e84bb7ba20c4a7b2f8
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-17 18:04:36 +0800

    chapter 2 exercise 37

---
+++ chapter2/2.37.scm
@@ -0,0 +1,12 @@
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (martix-*-vector m v)
+  (map (lambda (x) (dot-product x v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (martix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (v) (martix-*-vector cols v)) m)))
