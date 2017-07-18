2ad25e874b41e555b67dfde7bd5ea41af3ccdb50
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-18 14:56:59 +0800

    chapter 2 exercise 47

---
+++ chapter2/2.47.scm
@@ -0,0 +1,24 @@
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (caddr frame))
+
+
+(define (make-frame origin edge2 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (cddr frame))
