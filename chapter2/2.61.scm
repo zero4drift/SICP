eccc934419058071f115b3f3621fafdef59ede22
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-19 20:51:24 +0800

    chapter 2 exercise 61

---
+++ chapter2/2.61.scm
@@ -0,0 +1,12 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((= x (car set)) true)
+	((< x (car set)) false)
+	(else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+;; basically it is mathematical expection
