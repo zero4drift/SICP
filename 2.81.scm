c324b1be0e6b7f3b999d86770af83a44150a07f4
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-21 18:54:12 +0800

    chapter 2 exercise 81

---
+++ 2.81.scm
@@ -0,0 +1,40 @@
+;; a
+
+;; for exp with scheme-number this will do its job
+;; for exp with complex it exceeds maximum recursion depth
+
+
+;; b
+
+;; no, it induces another problem of recursion depth exceeding
+;; apply-generic procedure would not work properly,
+;; with two same parameters and the corresponding transfer procedure installed
+;; it's operation exceeds the maximum recursion depth
+
+
+;; c
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+	  (apply proc (map contents args))
+	  (if (= (length args) 2)
+	      (let ((type1 (car type-tags))
+		    (type2 (cadr type-tags))
+		    (a1 (car args))
+		    (a2 (cadr args)))
+		(let ((t1->t2 (get-coercion type1 type2))
+		      (t2->t1 (get-coercion type2 type1)))
+		  (cond ((equal? type1 type2)
+			 (error "No method for these types"
+				(list op type-tags)))
+			(t1->t2
+			 (apply-generic op (t1->t2 a1) a2))
+			(t2->t1
+			 (apply-generic op a1 (t2->t1 a2)))
+			(else
+			 (error "No method for these types"
+				(list op type-tags))))))
+	      (error "No method for these types"
+		     (list op type-tags)))))))
