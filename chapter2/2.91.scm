029667a0d3e5274d286ca42500a97e0a990a9038
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-24 00:37:00 +0800

    chapter 2 exercise 91

---
+++ chapter2/2.91.scm
@@ -0,0 +1,25 @@
+(define (div-terms L1 L2)
+  (if (empty-termlist? L1)
+      (list (the-empty-termlist) (the-empty-termlist))
+      (let ((t1 (first-term L1))
+	    (t2 (first-term L2)))
+	(if (> (order t2) (order t1))
+	    (list (the-empty-termlist) L1)
+	    (let ((nwe-c (div (coeff t1) (coeff t2)))
+		  (new-o (- (order t1) (order t2))))
+	      (let ((rest-of-result
+		     (div-terms
+		      (sub-terms L1 (mul-terms L2 (list (make-term new-o new-c))))
+		      L2)))
+		(list (add-join-term (make-term new-o new-c)
+			   (car rest-of-result))
+		      (cadr rest-of-result))))))))
+
+(define (div-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (let ((t1 (term-list p1))
+	    (t2 (term-list p2)))
+	(make-poly (variable p1)
+		   (div-terms t1 t2)))
+      (error "Polys not in same var -- DIV-POLY"
+	     (list p1 p2))))
