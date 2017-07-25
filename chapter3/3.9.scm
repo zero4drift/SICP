664f99c9354e834f6f5b2326f57442cd8bd9dfba
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-25 20:49:17 +0800

    chapter 3 exercise 9

---
+++ chapter3/3.9.scm
@@ -0,0 +1,5 @@
+;; for the first factorial
+;; there would be n environments constructed, for there are n factorial procedures called in this recursive process
+
+;; for the second factorial
+;; there would always  be one environment
