6d343a555480705cf75f07271249c0f6539cf8f3
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-25 21:23:45 +0800

    chapter 3 exercise 10

---
+++ chapter3/3.10.scm
@@ -0,0 +1,6 @@
+;; the difference from the old one is that:
+;; when the new version is applied,
+;; one more environment is constructed for the lambda form let
+;; this environment's father environ is the run-time environ of make-withdraw
+
+;; illustrations and figures unavailable here
