06d908c3293c2e93327b88d4e93167fd86aa14fc
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-26 12:10:21 +0800

    chapter 3 exercise 10

---
+++ chapter3/3.11.scm
@@ -0,0 +1,4 @@
+;; Calling procedure make-account two times makes two different
+;; run-time environ with individual local variables: balance, withdraw, deposit, dispatch
+
+;; acc and acc2 share the topest environ
