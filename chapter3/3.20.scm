042d7c01c203e629bbc55e4329e80322bba56638
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-26 17:54:42 +0800

    chapter 3 exercise 20

---
+++ chapter3/3.20.scm
@@ -0,0 +1,4 @@
+;; the evolution of pair implemented by procedures
+;; behaves like the procedure in exercise 3.11
+
+;; everything is related to local environ, local variable and environ inheritance.
