b66effae790db7a90806fbddc846059229038408
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-26 15:10:25 +0800

    chapter 3 exercise 16

---
+++ chapter3/3.16.scm
@@ -0,0 +1,7 @@
+;; pairs may share sub-pairs
+
+;; apply this procedure to one certain pair to return 3 or 4 or 7 is easy
+;; for the case of no return value: deathloop
+;; just like the exercise 3.13
+;; to achieve deathloop, one certain pair must contains itself
+;; or such pair as a component of another pair
