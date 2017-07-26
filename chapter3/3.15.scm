4ffa649b5f5475f3b9db32e3d5125fb01d6b10e5
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-26 14:59:25 +0800

    chapter 3 exercise 15

---
+++ chapter3/3.15.scm
@@ -0,0 +1,6 @@
+;; key point:
+;; the car and cdr of z1 point to one certain pair
+;; changing of the car of z1 impacts the cdr of z1 at the same time
+
+;; the car and cdr of z2 point to different pairs
+;; though they looks the same
