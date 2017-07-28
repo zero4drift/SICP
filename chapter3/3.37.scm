866e7f56708e817a0c446adc10a2e0b496c78f71
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-28 19:21:01 +0800

    chapter 3 exercise 37

---
+++ chapter3/3.37.scm
@@ -0,0 +1,31 @@
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+	  x)
+      (cv 32)))
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter C))
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (cv x)
+  (let ((y (make-connector)))
+    (constant x y)))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier y z x)
+    z))
+
+(define (c- x y)
+  (let ((z (make-connector)))
+    (adder y z x)
+    z))
