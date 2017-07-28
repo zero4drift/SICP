f87aa383ab2bf7581e9999e7b264f945b463d89e
zero4drift <fang0052@e.ntu.edu.sg>
2017-07-28 20:14:47 +0800

    chapter 3 exercise 38

---
+++ chapter3/3.38.scm
@@ -0,0 +1,15 @@
+;; a
+
+;; six possible values
+;; purely labor work
+;; not complex but time consuming
+;; always made people frustrated and bored
+;; ignore here
+
+;; b
+
+;; if so
+;; the outputs have much more alternatives than the question a
+;; time sequence illustration...
+;; same reason as a
+;; figure ignored here
