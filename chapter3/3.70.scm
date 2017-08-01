b2e63c5bf16387a8ee0e5572b52d3ff6693e8e4d
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-01 15:45:47 +0800

    chapter 3 exercise 70

---
+++ chapter3/3.70.scm
@@ -0,0 +1,44 @@
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+	((stream-null? s2) s1)
+	(else
+	 (let ((s1car (stream-car s1))
+	       (s2car (stream-car s2)))
+	   (cond ((< (weight s1car) (weight s2car))
+		  (cons-stream s1car (merge-weighted (stream-cdr s1) s2)))
+		 ((> (weight s1car) (weight s2car))
+		  (cons-stream s2car (merge-weighted s1 (stream-cdr s2))))
+		 (else
+		  (cons-stream s1car
+			       (merge-weighted
+				(stream-cdr s1)
+				(stream-cdr s2)))))))))
+
+(define (weighted-pairs s t weight)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (merge-weighted
+    (stream-map (lambda (x) (list (stream-car s) x))
+		(stream-cdr t))
+    (weighted-pairs (stream-cdr s) (stream-cdr t) weight)
+    weight)))
+  
+;; a
+
+(define desired1
+  (weighted-pairs
+   integers
+   integers
+   (lambda (p) (+ (car p) (cadr p)))))
+
+;; b
+
+(define s (cons-stream 1 (merge (scale-stream s 2)
+				(merge (scale-stream s 3)
+				       (scale-stream s 5)))))
+
+(define desired2
+  (weighted-pairs
+   s
+   s
+   (lambda (p) (+ (* 2 (car p)) (* 3 (cadr p)) (* 5 (car p) (cadr p))))))
