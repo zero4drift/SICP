bb30594f33261d0f214ddf0fa8928aea09592877
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-02 14:48:09 +0800

    chapter 3 exercise 80

---
+++ chapter3/3.80.scm
@@ -0,0 +1,14 @@
+;; mathematical permutations
+
+(define (RLC r l c dt)
+  (lambda (vc0 il0)
+    (define vc (integral (delay dvc) vc0 dt))
+    (define il (integral (delay dil) il0 dt))
+    (define dvc (scale-stream il (- (/ 1 c))))
+    (define dil
+      (add-streams
+       (scale-stream vc (/ 1 l))
+       (scale-stream il (- (/ r l)))))
+    (cons
+     vc
+     il)))
