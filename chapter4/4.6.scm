df2e401af80365be9f8b71c064d180128858dab5
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-03 19:23:44 +0800

    chapter 4 exercise 6

---
+++ chapter4/4.6.scm
@@ -0,0 +1,33 @@
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let-exps exp)
+  (cdr exp))
+
+(define (let-bindings exp)
+  (car exp))
+
+(define (let-body exp)
+  (cadr exp))
+
+(define (var binding)
+  (car binding))
+
+(define (expression binding)
+  (cadr binding))
+
+(define (let->application exp)
+  (transfer (let-exps exp)))
+
+(define (transfer exp)
+  (let ((bindings (let-bindings exp))
+	(body (let-body exp)))
+    (let ((var-list (map var bindings))
+	  (exp-list (map expression bindings)))
+      (let ((proc (make-lambda var-list body)))
+	(cons proc exp-list)))))
+
+(define (eval-let exp env)
+  (eval (let->application exp) env))
+
+;; implement let? and eval-let in eval
