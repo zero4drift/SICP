a4f9b4873c78cde4aa093130c25bac2c15bee019
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-10 17:51:53 +0800

    chapter 4 exercise 21

--- chapter4/4.21.scm
+++ chapter4/4.21.scm
@@ -1,3 +1,42 @@
-;; What I have done these two days!
-;; another day wasted!
-;; TOMORROW I MUST WORK
+(lambda (n)
+  ((lambda (fact)
+     (fact fact n))
+   (lambda (ft k)
+     (if (= k 1)
+	 1
+	 (* k (ft ft (- k 1))))))
+  10)
+
+;; a
+
+((lambda (n)
+   ((lambda (fib)
+      (fib fib n))
+    (lambda (fb k)
+      (cond ((= k 0) 0)
+	    ((= k 1) 1)
+	    (else
+	     (+ (fb fb (- k 1))
+		(fb fb (- k 2))))))))
+ 3)
+
+;; b
+
+(define (f x)
+  (define (even? n)
+    (if (=n 0)
+	true
+	(odd? (- n 1))))
+  (define (odd? n)
+    (if (= n 0)
+	false
+	(even? (- n 1))))
+  (even? x))
+
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false (ev? ev? od? (- n 1))))))
