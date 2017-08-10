d6adfe6319a6acf72aa74b48c4ad574dd452c218
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-10 23:00:48 +0800

    chapter 4 exercise 24

---
+++ chapter4/4.24.scm
@@ -0,0 +1,5 @@
+;; appearantly the analyze version is more efficient than the original one
+;; cause that the analyze process is only needed one time for one exp
+;; it is necessary everytime in the original version
+
+;; the time usage comparison is ignored here
