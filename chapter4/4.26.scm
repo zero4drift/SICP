839d4511f64ae8c7a0ed30f267ae7e889d0daa22
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-11 22:18:22 +0800

    chapter 4 exercise 26

---
+++ chapter4/4.26.scm
@@ -0,0 +1,22 @@
+(define (unless? exp)
+  (tagged-list? exp 'unless))
+
+(define (unless-condition exp)
+  (cadr exp))
+
+(define (unless-usual-value exp)
+  (caddr exp))
+
+(define (unless-exceptional-value exp)
+  (cadddr exp))
+
+(define (unless-if exp)
+  (let ((condition (unless-condition exp))
+	(usual-value (unless-usual-value exp))
+	(usual-exceptional-value exp))
+    (make-if condition usual-exceptional-value usual-value)))
+
+(define (eval-unless exp env)
+  (eval (unless-if exp) env))
+
+;; implemetn unless? and eval-unless in eval
