6a988f1c918d98423a7d4f9ebc3dd91c62449497
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-14 19:39:35 +0800

    chapter 4 exercise 38

---
+++ 4.38.scm
@@ -0,0 +1,23 @@
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+	(cooper (amb 1 2 3 4 5))
+	(fletcher (amb 1 2 3 4 5))
+	(miller (amb 1 2 3 4 5))
+	(smith (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (require (not (= (abs (- fletcher miller)) 1)))
+    (list (list 'baker baker)
+	  (list 'cooper cooper)
+	  (list 'fletcher fletcher)
+	  (list 'miller miller)
+	  (list 'smith smith))))
+
+;; do not make me do mathematical arrangements
