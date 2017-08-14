c9b4a317098b9bdd2bda617b2d47fae8bcafd994
zero4drift <fang0052@e.ntu.edu.sg>
2017-08-14 19:02:37 +0800

    chapter 4 exercise 37

---
+++ 4.37.scm
@@ -0,0 +1,7 @@
+;; I think it is as efficient as the one of exercise 4.35
+;; cause the predication and computation of k
+;; is theoretically a iteration of k
+;; which makes no difference compared to the one in exercise 4.35
+
+;; though (require (>= hsq ksq)) and (require (integer? k)) may filter some values of k
+;; considered from bigger scale, the conputation work will not be significantly reduced
