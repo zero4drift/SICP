6785467142d5570cbdff2ffe485ce9896fce085a
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-15 21:53:59 +0800

    Make procedures in exercise 1.12 all iterative compute processes.

--- chapter1/1.12.scm
+++ chapter1/1.12.scm
@@ -12,6 +12,11 @@
 		     (factorial (- row col)))))))
 
 (define (factorial n)
-  (if (= n 1)
-      1
-      (* n (factorial (- n 1)))))
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+		 (+ counter 1)
+		 max-count)))
