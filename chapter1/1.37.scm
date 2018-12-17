13d9befa8bc106c0f3f5dc862c09398346bd4430
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-17 18:02:08 +0800

    Fix exercise 1.37.

--- chapter1/1.37.scm
+++ chapter1/1.37.scm
@@ -1,29 +1,30 @@
-;; 1.6180...
+;; 0.6180...
 
 ;; recursive
 
 (define (cont-frac n d k)
   (define (recursive count)
-    (if (> count k)
+    (if (= count k)
 	(/ (n count) (d count))
 	(/ (n count) (+ (d count) (recursive (+ count 1))))))
-  (recursive 0))
+  (recursive 1))
 
-(/ 1 (cont-frac (lambda (i) 1.0)
-		(lambda (i) 1.0)
-		10))			; generates 1.618055... after 10 steps
+(cont-frac (lambda (i) 1.0)
+	   (lambda (i) 1.0)
+	   11)			; generates 0.6180555... after 11 steps
 
 ;; iterative
 
 (define (cont-frac-iter n d k)
   (define (iter count result)
     (if (= count k)
-	result
+	(/ (n count)
+	   (+ (d count) result))
 	(iter (+ count 1)
 	      (/ (n count)
 		 (+ (d count) result)))))
-  (iter 0 0))
+  (iter 1 0))
 
 (/ 1 (cont-frac-iter (lambda (i) 1.0)
 		     (lambda (i) 1.0)
-		     10))		;also requires 10 steps
+		     11))		;also requires 11 steps
