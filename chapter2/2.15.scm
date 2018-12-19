aebaedfeb3d71bf149950c671d151e969c26b930
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-19 13:48:49 +0800

    Best answer of ex 2.15 & 2.16 is 'Uncertainty accumulated'.

--- chapter2/2.15.scm
+++ chapter2/2.15.scm
@@ -1,5 +1,7 @@
+;; in short: uncertainty accumulated;
+
 ;; execise 2.14 proves that operations,
 ;; such as add/sub/mul/div of intervals enlarge the range of the result interval,
 ;; which makes the result less precise,
-;; manily due to the inherent uncertanity of interval itself;
+;; manily due to the inherent uncertainty of interval itself;
 ;; the equation with less intervals computes more precise result
