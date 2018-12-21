f2b00b0f9e2191619ad3db86f0c716078d13feb7
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-21 16:33:11 +0800

    Fix ex 2.53.scm.

--- chapter2/2.53.scm
+++ chapter2/2.53.scm
@@ -1,18 +1,18 @@
 (list 'a 'b 'c)				;(a b c)
 
-(list (list 'george))			;(george)
+(list (list 'george))			;((george))
 
-(cdr '((x1 x2) (y1 y2)))		;(y1 y2)
+(cdr '((x1 x2) (y1 y2)))		;((y1 y2))
 
 (cadr '((x1 x2) (y1 y2)))		;(y1 y2)
 
 (pair? (car '(a short list)))		;false
 
 (define (memq item x)
-  (cond ((null? x) false)
+  (cond ((null? x) #f)
 	((eq? item (car x)) x)
 	(else (memq item (cdr x)))))
 
-(memq 'red '((red shoes) (blue socks)))	;false
+(memq 'red '((red shoes) (blue socks)))	;#f
 
 (memq 'red '(red shoes blue socks))	;(red shoes blue socks)
