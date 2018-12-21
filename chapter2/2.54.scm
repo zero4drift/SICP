dc5d97455113e1842caec01051ca4c4562e4daf9
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-21 16:41:08 +0800

    Replace bool value true and false with #t and #f in ex 2.54.

--- chapter2/2.54.scm
+++ chapter2/2.54.scm
@@ -5,5 +5,5 @@
 	  (equal? (cdr items1) (cdr items2))))
 	((and (symbol? items1) (symbol? items2))
 	 (eq? items1 items2))
-	((and (null? items1) (null? items2)) true)
-	(else false)))
+	((and (null? items1) (null? items2)) #t)
+	(else #f)))
