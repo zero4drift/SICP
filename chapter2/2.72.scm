97f15519a066cd5cbc049a6a710ed74b599fa925
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-22 21:09:57 +0800

    Add comment in ex 2.72.

--- chapter2/2.72.scm
+++ chapter2/2.72.scm
@@ -1,5 +1,5 @@
-;; the increasing speed of steps
+;; the increasing speed of steps theta(n) (theory referred by CLRS section 4.5)
 
-;; the most frequency one: O(1)
+;; the most frequency one: theta(1)
 
 ;; the least frequency one: O((expt n 2)) due to the memq operation in every search deeper
