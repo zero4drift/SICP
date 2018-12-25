9b91bbf11a2bbefce5bb3a634ef8082a9c1c6023
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-25 21:01:36 +0800

    Improve answer of ex 2.80.

--- chapter2/2.80.scm
+++ chapter2/2.80.scm
@@ -1,17 +1,18 @@
+;; below three separate statements better put inside install procedure
 (define (=zero-number? n)
   (= n 0))
+(put '=zero? '(scheme-number) =zero-number?)
 
 (define (=zero-rational? r)
   (= (numer r) 0))
+(put '=zero? '(rational) =zero-rational?)
 
 (define (=zero-complex? c)
   (let ((real (real-part c))
 	(imag (imag-part c)))
     (and (= real 0) (= imag 0))))
+(put '=zero? '(complex) =zero-complex?)
 
-(put 'equ? 'scheme-number =zero-number?)
-(put 'equ? 'rational =zero-rational?)
-(put 'equ? 'complex =zero-complex?)
-
+;; general =zero? interface
 (define (=zero? x)
   (apply-generic '=zero? x))
