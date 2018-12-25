d2f095a9d6c68b3602928c2444a4714501cf03a9
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-25 16:51:05 +0800

    Improve answer of ex 2.77.

--- chapter2/2.77.scm
+++ chapter2/2.77.scm
@@ -5,10 +5,13 @@
 ;; (magnitude z)
 ;; (apply-generic 'magnitude z)
 ;; (apply (get 'magnitude '(complex)) (map contents z))
-;; (apply magnitude (cons 'rectangular (3 4)))
+;; (apply magnitude (cons 'rectangular (cons 3 4)))
 ;; (apply-generic 'magnitude (cons 'rectangular (cons 3 4)))
 ;; (apply (get 'magnitude '(rectangular)) (cons 3 4))
-;; (magnitude (cons 3 4)
+;; (magnitude (cons 3 4))
+;; (sqrt (+ (square (real-part (cons 3 4))) (square (imag-part (cons 3 4)))))
 ;; ...
 
 ;; apply-generic is called two times
+;; the procedure firstly dispatched is magnitude of complex package
+;; the procedure secondly dispatched is magnitude of rectangular package
