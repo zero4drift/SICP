d8fe290a5d309798b650e844f95e20888504de4f
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-26 22:22:20 +0800

    Add comments and improve answer of ex 2.90.

--- chapter2/2.90.scm
+++ chapter2/2.90.scm
@@ -9,51 +9,82 @@
 
 (define (install-dense-terms-package)
   ;; procedures about dense terms...
+  ;; procedure rest-terms? and so on, define and put
   (define (tag terms)
     (attach-tag 'dense terms))
-  (define (minus-t terms)
+  (define (first-term term-list)
+    (make-term (- (len term-list) 1) (car term-list)))
+  (define (adjoin-term term term-list)
+    (cond ((=zero? (coeff term)) term-list)
+	  ((=equ? (order term) (length term-list))
+	   (cons (coeff term) term-list))
+	  ((> (order term) (length term-list))
+	   (adjoin-term term (cons 0 term-list))))) ;
+  (define (negative-t terms)
     (if (empty-termlist? terms)
 	the-empty-termlist
-	(let ((first (first-term terms)))
-	  (adjoin-term (make-term
-			(order term)
-			(minus (coeff term)))
-		       (minus-t (rest-terms terms))))))
-  (put 'make 'dense
-       (lambda (x) (tag x)))
-  (put 'minus 'dense
-       (lambda (x) (tag (minus x))))
-  (put 'first-term
-       (lambda (x) (first-term x)))
-  (put 'adjoin-term '(dense)
-       (lambda (x y) (tag (adjoin-term x y)))))
+	(let ((first (car terms)))
+	  (const (negative first)
+		 negative-t (rest-terms terms))))
+    (put 'make 'dense
+	 (lambda (x) (tag x)))
+    (put 'negative '(dense)
+	 (lambda (x) (tag (negative x))))
+    (put 'first-term '(dense)
+	 (lambda (x) (first-term x)))
+    (put 'adjoin-term 'dense
+	 (lambda (x y) (tag (adjoin-term x y))))))
 
 (define (make-dense-terms terms)
   ((get 'make 'dense terms) terms))
 
+
 (define (install-sparse-terms-package)
   ;; procedures about sparse terms...
+  ;; procedure rest-terms? and so on, define and put
   (define (tag terms)
     (attach-tag 'sparse terms))
-  (define (minus-t terms)
+  (define (first-term term-list) (car term-list))
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+	term-list
+	(cons term term-list)))
+  (define (negative-t terms)
     (if (empty-termlist? terms)
-	(the-empty-termlist)
-	(adjoin-term
-	 (make-term
-	  (minus (first-term terms)))
-	 (minus-t (rest-terms terms)))))
+	the-empty-termlist
+	(let ((first (first-term terms)))
+	  (adjoin-term (make-term
+			(order first)
+			(negative (coeff first)))
+		       (negative-t (rest-terms terms))))))
   (put 'make 'sparse
        (lambda (x) (tag x)))
-  (put 'minus 'sparse
-       (lambda (x) (tag (minus-t x))))
-  (put 'first-term 'sparse
+  (put 'negative '(sparse)
+       (lambda (x) (tag (negative-t x))))
+  (put 'first-term '(sparse)
        (lambda (x) (first-term x)))
   (put 'adjoin-term 'sparse
        (lambda (x y) (tag (adjoin-term x y)))))
 
 (define (make-sparse-terms terms)
   ((get 'make 'sparse) terms))
-  
+
+
+(define (first-term x)
+  (apply-generic 'first-term x))
+
+(define (adjoin-term x y)
+  ((get 'adjoin-term (type-tag y)) x (contents y)))
+
+(define (negative x)
+  (apply-generic 'negative x))
+
+;; add-terms
+;; mul-terms
+;; sub-terms
+;; add-poly
+;; mul-poly
+;; sub-poly
 
 (define (install-polynomial-package)
   ;; internal procedures...
@@ -61,12 +92,3 @@
        (lambda (x y) (tag 'poly (sub-poly x y)))) ;2.88
   ;; others kept unchanged
   )
-
-(define (first-term x)
-  (apply-generic 'first-term x))
-
-(define (adjoin-term x y)
-  ((get 'adjoin-term (tag-type y)) x (contents y)))
-
-(define (minus x)
-  (apply-generic 'minus x))
