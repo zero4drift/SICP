25ed8ad122e8d786309cad44e131ef0e11b3f34b
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-26 20:25:36 +0800

    Fix typo of answer 2.88.

--- chapter2/2.88.scm
+++ chapter2/2.88.scm
@@ -1,34 +1,34 @@
-(define (install-minus)
-  (define (minus-scheme-number n)
+(define (install-negative)
+  (define (negative-scheme-number n)
     (make-scheme-number (- n)))
 
-  (define (minus-rational n)
-    (make-rational (minus (numer n)) (denom n)))
+  (define (negative-rational n)
+    (make-rational (negative (numer n)) (denom n)))
 
-  (define (minus-complex n)
-    (make-complex-from-real-imag (minus (real-part n)) (minus (imag-part n))))
+  (define (negative-complex n)
+    (make-complex-from-real-imag (negative (real-part n)) (negative (imag-part n))))
 
-  (define (minus-poly n)
+  (define (negative-poly n)
     (define (recursive-t terms)
       (if (empty-termlist? terms)
 	  the-empty-termlist
 	  (let ((first (first-term terms)))
 	    (adjoin-term (make-term
-		   (order term)
-		   (minus (coeff term)))
-		  (recursive-t (rest-terms terms))))))
+			  (order first)
+			  (negative (coeff first)))
+			 (recursive-t (rest-terms terms))))))
     (make-poly (variable n) (recursive-t (term-list n))))
 
-  (put 'minus 'scheme-number minus-scheme-number)
-  (put 'minus 'rational minus-rational)
-  (put 'minus 'complex minus-complex)
-  (put 'minus 'polynomial minus-poly))
+  (put 'negative '(scheme-number) negative-scheme-number)
+  (put 'negative '(rational) negative-rational)
+  (put 'negative '(complex) negative-complex)
+  (put 'negative '(polynomial) negative-poly))
 
-(define (minus n)
-  (apply-generic 'minus n))
+(define (negative n)
+  (apply-generic 'negative n))
 
 (define (sub-poly p1 p2)
-  (add-poly p1 (minus p2)))
+  (add-poly p1 (negative p2)))
 
 ;; add this into install-polynomial-package
 (put 'sub '(polynomial polynomial)
