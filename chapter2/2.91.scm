185a48b02b33540376f810a54900127e55b26815
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-27 15:10:55 +0800

    Fix bug and typo, improve answer of ex 2.91.

--- chapter2/2.91.scm
+++ chapter2/2.91.scm
@@ -9,17 +9,21 @@
 		  (new-o (- (order t1) (order t2))))
 	      (let ((rest-of-result
 		     (div-terms
-		      (sub-terms L1 (mul-terms L2 (list (make-term new-o new-c))))
+		      (sub-terms L1 (mul-term-by-all-terms (make-term new-o new-c) L2))
 		      L2)))
-		(list (add-join-term (make-term new-o new-c)
-			   (car rest-of-result))
+		(list (addjoin-term (make-term new-o new-c)
+				    (car rest-of-result))
 		      (cadr rest-of-result))))))))
 
 (define (div-poly p1 p2)
   (if (same-variable? (variable p1) (variable p2))
       (let ((t1 (term-list p1))
 	    (t2 (term-list p2)))
-	(make-poly (variable p1)
-		   (div-terms t1 t2)))
+	(let ((result (div-terms t1 t2)))
+	  (list
+	   (make-poly (variable p1)
+		      (car result))
+	   (make-poly (variable p1)
+		      (cadr result)))))
       (error "Polys not in same var -- DIV-POLY"
 	     (list p1 p2))))
