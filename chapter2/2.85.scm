12753a88dd8de21770f386154dcaf81a07470af4
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-28 17:37:20 +0800

    Fix bug and improve answer of ex 2.85.

--- chapter2/2.85.scm
+++ chapter2/2.85.scm
@@ -18,44 +18,20 @@
   (apply-generic 'project x))
 
 ;; install drop
-(define (drop-scheme-number x)
-  x)
-
-(define (drop-rational x)
-  (let ((a (project x)))
-    (if (equ? (raise a) x)
-	a
-	x)))
-
-(define (drop-real x)
-  (let ((a (project x)))
-    (if (equ? (raise (raise a)) x)
-	a
-	x)))
-
-(define (drop-complex x)
-  (let ((a (project x)))
-    (if (equ? (raise a) x)
-	(drop a)
-	x)))	
-
-(define (install-drop)
-  (put 'drop '(scheme-number) drop-scheme-number)
-  (put 'drop '(rational) drop-rational)
-  (put 'drop '(real) drop-real)
-  (put 'drop '(complex) drop-complex)
-  'done)
-
-(define (drop n)
-  (apply-generic 'drop n))
-
+(define (drop x)
+  (if (number? x)
+      x
+      (let ((a (project x)))
+	(if (equ? (raise a) x)
+	    (drop a)
+	    x))))	
 
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
       (if proc	      
 	  (let ((res (apply proc (map contents args))))
-	    (if (or (eq? op 'raise) (eq? op 'equ?) (eq? op 'drop))
+	    (if (or (eq? op 'raise) (eq? op 'equ?) (eq? op '=zero?))
 		res
 		(drop res)))
 	  (if (= (length args) 2)
@@ -66,7 +42,7 @@
 		(cond ((equal? type1 type2)
 		       (error "No method for these types"
 			      (list op type-tags)))
-		      ((lower-one t1 t2)
+		      ((lower-one type1 type2)
 		       (apply-generic op (raise a1) a2))
 		      (else (apply-generic op a1 (raise a2)))))
 	      (error "No method for these types"
