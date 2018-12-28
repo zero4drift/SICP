8422c77f1540e1f1f3964baa989a7fb23608d35f
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-28 20:36:52 +0800

    Fix bug of ex 3.24.

--- chapter2/2.87.scm
+++ chapter2/2.87.scm
@@ -2,13 +2,13 @@
   (define (=zero?-p p)
     (define (recursive terms)
       (if (empty-termlist? terms)
-	  true
+	  #t
 	  (let ((first (first-term terms))
 		(rest (rest-terms terms)))
 	    (if (=zero? (coeff first))
 		(recursive rest)
-		false))))
+		#f))))
     (recursive (term-list p)))
-  (put '=zero? 'polynomial =zero?-p))
+  (put '=zero? '(polynomial) =zero?-p)
 
 (install-poly-zero?)
