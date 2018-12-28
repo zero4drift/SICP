72667d6fb7ad31a1390c5031970237daa9130eec
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-28 20:21:35 +0800

    polynomial package workable.

--- chapter2/systems-with-generic-operations.scm
+++ chapter2/systems-with-generic-operations.scm
@@ -70,12 +70,12 @@
     (let ((proc (get op type-tags)))
       (if proc
 	  ;; follow 2.85
-	  (let ((res (apply proc (map contents args))))
-	    (if (or (eq? op 'raise) (eq? op 'equ?) (eq? op '=zero?))
-	  	res
-	  	(drop res)))
+	  ;; (let ((res (apply proc (map contents args))))
+	  ;;   (if (or (eq? op 'raise) (eq? op 'equ?) (eq? op '=zero?))
+	  ;; 	res
+	  ;; 	(drop res)))
 	  ;; 2.85
-	  ;; (apply proc (map contents args))
+	  (apply proc (map contents args))
 	  (if (= (length args) 2)
 	      (let ((type1 (car type-tags))
 		    (type2 (cadr type-tags))
@@ -332,7 +332,7 @@
   (define (make-from-real-imag x y) (cons x y))
   (define (magnitude z)
     (sqr (add (square (real-part z))
-	       (square (imag-part z)))))
+	      (square (imag-part z)))))
   (define (angle z)
     (atan (imag-part z) (real-part z)))
   (define (make-from-mag-ang r a)
@@ -375,7 +375,6 @@
        (lambda (r a) (tag (make-from-mag-ang r a))))
   'done)
 
-
 (define (real-part z) (apply-generic 'real-part z))
 (define (imag-part z) (apply-generic 'imag-part z))
 (define (magnitude z) (apply-generic 'magnitude z))
@@ -386,6 +385,96 @@
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
 
+;; polynomial-package
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+  ;; representation of terms and term lists  
+  (define (adjoin-term term terms)
+    (if (=zero? (coeff term))
+	terms
+	(cons term terms)))
+  (define (first-term terms) (car terms))
+  (define (rest-terms terms) (cdr terms))
+  (define (empty-termlist? terms) (null? terms))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1)) (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1 (add-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term t2 (add-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term (order t1)
+                                (add (coeff t1) (coeff t2)))
+                     (add-terms (rest-terms L1)
+                                (rest-terms L2)))))))))
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        L1
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        L
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term (add (order t1) (order t2))
+                      (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+
+  ;; poly
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1)
+                              (term-list p2)))
+	(error "Polys not in same var -- ADD-POLY"
+	       (list p1 p2))))
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1)
+			      (term-list p2)))
+	(error "Polys not in same var -- MUL-POLY"
+	       (list p1 p2))))
+  
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  ;; (put 'sub '(polynomial polynomial)
+  ;; (lambda (p1 p2) (tag (add-poly p1 (negate-poly p2)))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  ;; (put 'div '(polynomial polynomial) (lambda (p1 p2) (tag (div-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  ;; (put 'make-term 'polynomial make-term)
+  ;; (put 'order 'polynomial order)
+  ;; (put 'coeff 'polynomial coeff)
+  ;; (put 'negate '(polynomial) (lambda (p) (tag (negate-poly p))))
+  ;; (put 'greatest-common-divisor '(polynomial polynomial) (lambda (a b) (tag (gcd-poly a b))))
+  ;; (put 'reduce '(polynomial polynomial) (lambda (a b) (map tag (reduce-poly a b)))) 
+  'done)
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+
 
 ;; install
 (install-types-tower)
@@ -394,3 +483,4 @@
 (install-rectangular-package)
 (install-polar-package)
 (install-complex-package)
+(install-polynomial-package)
