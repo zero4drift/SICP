88a6cd64b2589f239af8e2838d6c326b21c3f80f
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-28 15:25:35 +0800

    Integrate ex 2.81.

--- chapter2/systems-with-generic-operations.scm
+++ chapter2/systems-with-generic-operations.scm
@@ -27,14 +27,32 @@
 	 (error "Bad tagged datum -- CONTENTS" datum))))
 ;; 2.78
 
+;; follow 2.81
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
       (if proc
 	  (apply proc (map contents args))
-	  (error
-	   "No method for these types -- APPLY-GENERIC"
-	   (list op type-tags))))))
+	  (if (= (length args) 2)
+	      (let ((type1 (car type-tags))
+		    (type2 (cadr type-tags))
+		    (a1 (car args))
+		    (a2 (cadr args)))
+		(if(equal? type1 type2)
+		   (error "No method for these types"
+			  (list op type-tags))
+		   (let ((t1->t2 (get-coercion type1 type2))
+			 (t2->t1 (get-coercion type2 type1)))
+		     (cond (t1->t2
+			    (apply-generic op (t1->t2 a1) a2))
+			   (t2->t1
+			    (apply-generic op a1 (t2->t1 a2)))
+			   (else
+			    (error "No method for these types"
+				   (list op type-tags))))))
+		(error "No method for these types"
+		       (list op type-tags))))))))
+;; 2.81
 
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
