a7991e0c3e954b913b365307f7ed65988e8fe5ba
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-28 23:22:23 +0800

    Fix typo of ex 2.89.

--- chapter2/2.89.scm
+++ chapter2/2.89.scm
@@ -1,11 +1,11 @@
 ;; keep other procedures unchanged
 
-(define (first-term term-list)
-  (make-term (- (len term-list) 1) (car term-list)))
+(define (first-term-d term-list)
+    (make-term (- (length term-list) 1) (car term-list)))
 
-(define (adjoin-term term term-list)
-  (cond ((=zero? (coeff term)) term-list)
-	((=equ? (order term) (length term-list))
-	 (cons (coeff term) term-list))
-	((> (order term) (length term-list))
-	 (adjoin-term term (cons 0 term-list))))) ;the order of term would never be smaller than the length
+(define (adjoin-term-d term term-list)
+    (cond ((=zero? (coeff term)) term-list)
+	  ((equ? (order term) (length term-list))
+	   (cons (coeff term) term-list))
+	  ((> (order term) (length term-list))
+	   (adjoin-term-d term (cons 0 term-list))))) ;;the order of term would never be smaller than the length
