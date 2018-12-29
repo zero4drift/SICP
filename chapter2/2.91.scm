a47e16cdfe3052274be0cf0a6987337a45bdc98a
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-29 12:08:11 +0800

    Use new interface: make-empty-termlist, fix bug and typo.

--- chapter2/2.91.scm
+++ chapter2/2.91.scm
@@ -1,18 +1,21 @@
 (define (div-terms L1 L2)
   (if (empty-termlist? L1)
-      (list (the-empty-termlist) (the-empty-termlist))
+      (list L1 L1)
       (let ((t1 (first-term L1))
 	    (t2 (first-term L2)))
 	(if (> (order t2) (order t1))
-	    (list (the-empty-termlist) L1)
-	    (let ((nwe-c (div (coeff t1) (coeff t2)))
+	    (list (make-empty-termlist L1) L1)
+	    (let ((new-c (div (coeff t1) (coeff t2)))
 		  (new-o (- (order t1) (order t2))))
 	      (let ((rest-of-result
 		     (div-terms
-		      (sub-terms L1 (mul-term-by-all-terms (make-term new-o new-c) L2))
+		      (add-terms L1
+				 (negative (mul-term-by-all-terms
+					    (make-term new-o new-c)
+					    L2)))
 		      L2)))
-		(list (addjoin-term (make-term new-o new-c)
-				    (car rest-of-result))
+		(list (adjoin-term (make-term new-o new-c)
+				   (car rest-of-result))
 		      (cadr rest-of-result))))))))
 
 ;; the remainder of result of div-terms should be deprecated in div-poly
@@ -25,3 +28,4 @@
 		     (car result))))
       (error "Polys not in same var -- DIV-POLY"
 	     (list p1 p2))))
+(put 'div '(polynomial polynomial)
