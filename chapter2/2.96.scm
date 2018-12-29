1fc544e0ff627a78ebdbc2800afcd03803a5adfc
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-29 16:22:22 +0800

    Fix bug and refactoring of ex 2.96.

--- chapter2/2.96.scm
+++ chapter2/2.96.scm
@@ -1,5 +1,21 @@
 ;; only for polynomial with integer coeffs.
 
+;; inside install saprse procedure
+(define (coeffs-s terms)
+  (map (lambda (t) (coeff t)) terms))
+(put 'coeffs '(polynomial-sparse) coeffs-s)
+
+;; inside install dense procedure
+(define (coeffs-d terms)
+  (cond ((empty-termlist-d? terms) '())
+	((equ? (car terms) 0) (coeffs-d (cdr terms)))
+	(else (cons (car terms) (coeffs-d (cdr terms))))))
+(put 'coeffs '(polynomial-dense) coeffs-d)
+
+;; generic operation
+(define (coeffs t)
+  (apply-generic 'coeffs t))
+
 ;; a
 (define (pseudoremainder-terms a b)
   (let ((f1 (first-term a))
@@ -7,13 +23,12 @@
     (let ((o1 (order f1))
 	  (o2 (order f2))
 	  (c (coeff f2)))
-      (let ((constant (expt c (+ 1 o1 (- o2)))))
+      (let ((constant (exp c (+ 1 o1 (- o2)))))
 	(let ((term (make-term 0
 			       constant)))
 	  (cadr (div-terms
 		 (mul-term-by-all-terms term a)
 		 b)))))))
-	       
 
 (define (gcd-terms a b)
   (if (empty-termlist? b)
@@ -22,21 +37,20 @@
 
 ;; b
 (define (gcd-terms-coeff terms)
-    (let ((coeffs (map (lambda (t) (coeff t)) terms)))
-      (define (recursive coeffs)
-	(if (null? (cddr coeffs))
-	    (greatest-common-divisor
-	     (car coeffs)
-	     (cadr coeffs))
-	    (greatest-common-divisor
-	     (car coeffs)
-	     (recursive coeffs))))
-      (recursive coeffs)))
+  (define (recursive coeffs)
+    (if (null? (cddr coeffs))
+	(greatest-common-divisor
+	 (car coeffs)
+	 (cadr coeffs))
+	(greatest-common-divisor
+	 (car coeffs)
+	 (recursive (cdr coeffs)))))
+  (recursive (coeffs terms)))
 
 (define (gcd-terms a b)
   (if (empty-termlist? b)
       (let ((coeffs-gcd (gcd-terms-coeff a)))
-	(div-terms
-	 a
-	 (list (make-term 0 coeffs-gcd))))
+	(mul-term-by-all-terms 
+	 (make-term 0 (div 1 coeffs-gcd))
+	 a))
       (gcd-terms b (pseudoremainder-terms a b))))
