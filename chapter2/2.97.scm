be95f6405711303c2ae3a4c932407d0019a82c58
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-29 21:08:40 +0800

    Use direct div-terms instead of mul-terms with reciprocal, fix typo

--- chapter2/2.97.scm
+++ chapter2/2.97.scm
@@ -9,12 +9,11 @@
       (let ((c (coeff af))
 	    (o1 (max (order bf) (order cf)))
 	    (o2 (order af)))
-	(expt c (+ 1 o1 (- o2))))))
+	(exp c (+ 1 o1 (- o2))))))
   (define (simplify a b)
-    (let ((g (gcd-terms-coeff (append a b))))
-      (let ((sa (car (div-terms a (list (make-term 0 g)))))
-	    (sb (car (div-terms b (list (make-term 0 g))))))
-	(list sa sb))))
+    (let ((g (gcd-terms-coeff (append (coeffs a) (coeffs b)))))
+      (let ((divider (make-terms (type-tag a) (list (make-term 0 g)))))
+	(list (car (div-terms a divider)) (car (div-terms b divider))))))
   (let ((g (gcd-terms n d)))
     (let ((constant (compute-constant g n d)))
       (let ((mn (mul-term-by-all-terms (make-term 0 constant) n))
@@ -22,9 +21,8 @@
 	(let ((gn (car (div-terms mn g)))
 	      (gd (car (div-terms md g))))
 	  (simplify gn gd))))))	
-
 (define (reduce-poly p1 p2)
-  (if (same-vaiable? (variable p1) (variable p2))
+  (if (same-variable? (variable p1) (variable p2))
       (let ((t1 (term-list p1))
 	    (t2 (term-list p2)))
 	(let ((result (reduce-terms t1 t2)))
@@ -32,6 +30,8 @@
 		(make-poly (variable p1) (cadr result)))))
       (error "Polys not in same variable -- REDUCE-POLY"
 	     (list p1 p2))))
+(put 'reduce '(polynomial polynomial)
+     (lambda (n d) (map tag (reduce-poly n d))))
 
 ;; b
 
