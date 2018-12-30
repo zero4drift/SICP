d8bd9cfae252a53ea8f2d4cdeba9f8ddcced08b5
zero4drift <fang0052@e.ntu.edu.sg>
2018-12-30 17:08:59 +0800

    Add 'reset-count' to ex 3.2.

--- chapter3/3.2.scm
+++ chapter3/3.2.scm
@@ -1,7 +1,8 @@
 (define (make-monitored f)
   (let ((count 0))
     (lambda (x)
-      (if (eq? x 'how-many-calls?)
-	  count
-	  (begin (set! count (+ count 1))
-		 (f x))))))
+      (cond
+       ((eq? x 'how-many-calls?) count)
+       ((eq? x 'reset-count) (set! count 0))
+       (else (begin (set! count (+ count 1))
+		    (f x)))))))
