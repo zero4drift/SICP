fb266849cb46b028b3623a19aa764ee8eb9cbad9
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-01 15:16:11 +0800

    Run program of ex 3.14 to prove the answer.

--- chapter3/3.14.scm
+++ chapter3/3.14.scm
@@ -1,3 +1,14 @@
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+	y
+	(let ((temp (cdr x)))
+	  (set-cdr! x y)
+	  (loop temp x))))
+  (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+
 ;; reverse the second list parameter and return the result
 ;; v: (list 'a 'b 'c 'd)
 ;; w: (list 'd 'c 'b 'a)
