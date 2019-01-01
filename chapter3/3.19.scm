61a33515b1f795f5a67d20a3ad44cd032ca59ac6
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-01 17:09:54 +0800

    Still could not figure out the underneath theory of the solution.

--- chapter3/3.19.scm
+++ chapter3/3.19.scm
@@ -1,19 +1,14 @@
-;; requiere the memq procedure of exercise 3.17.scm
+;; refer to http://en.wikipedia.org/wiki/Cycle_detection
 
-(define (list-walk steps list)
-  (cond ((null? list) '())
-	((= steps 0) list)
-	(else
-	 (list-walk
-	  (- steps 1)
-	  (cdr list)))))
-
-(define (clever-infinite? l)
-  (define (iter x y)
-    (let ((a (list-walk 1 x))
-	  (b (list-walk 2 y)))
-      (cond ((or (null? a) (null? b)) false)
-	    ((eq? a b) true)
-	    (else
-	     (iter a b)))))
-  (iter l l))
+(define (contains-cycle? lst) 
+  (define (safe-cdr l) 
+    (if (pair? l) 
+        (cdr l) 
+        '())) 
+  (define (iter a b) 
+    (cond ((not (pair? a)) #f) 
+          ((not (pair? b)) #f) 
+          ((eq? a b) #t) 
+          ((eq? a (safe-cdr b)) #t) 
+          (else (iter (safe-cdr a) (safe-cdr (safe-cdr b)))))) 
+  (iter (safe-cdr lst) (safe-cdr (safe-cdr lst)))) 
