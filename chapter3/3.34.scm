2e32a80b6bc0c54284a8adbb0c57d11d4554b3ac
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-02 21:46:11 +0800

    Give a new explaination in ex 3.34.

--- chapter3/3.34.scm
+++ chapter3/3.34.scm
@@ -1,11 +1,9 @@
 (define (squarer a b)
   (multiplier a a b))
 
-;; bug
-;; still this defined multiplier inside squarer
-;; has just one input and output
-
-;; because in procedure make-connector
-;; the connect proc has if predicate statement
-;; that the constraint connected before
-;; could not be connected again
+;; set value of a, the value of b could be computed;
+;; while set value of b, the value of a could not be.
+;; because when we want to compute the 2nd root of b,
+;; we must set value of b and make a forget its value(or the new value could not be set),
+;; the cond predicate statements of procedure process-new-value of multiplier all failed,
+;; it is impossibile to set a new value for a.
