17a8b4f75422893f04b7b82a1626daedeb912686
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-04 22:01:30 +0800

    Add an explaination in ex 3.58.

--- chapter3/3.58.scm
+++ chapter3/3.58.scm
@@ -1,3 +1,4 @@
+;; The result is the rational value that num divides den in base radix(num < den)
 (define (expand num den radix)
   (cons-stream
    (quotient (* num radix) den)
@@ -5,6 +6,7 @@
 
 ;; (expand 1 7 10)
 ;; (1 4 2 8 5 ...)
+;; 1 / 7 = 0.14285...
 
 ;; (expand 3 8 10)
 ;; (3 7 5 0 0 ...)
