86a701cc206020fb5771b149653be11d91bfae23
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-04 18:04:27 +0800

    Make a more detailed answer of ex 3.52.

--- chapter3/3.52.scm
+++ chapter3/3.52.scm
@@ -1,14 +1,35 @@
-(define sum 0)				;sum 0
+(define sum 0)
+;; sum 0
+
 (define (accum x)
   (set! sum (+ x sum))
-  sum)					;sum 0
-(define seq (stream-map accum (stream-enumerate-interval 1 20))) ;sum 1
-(define y (stream-filter even? seq))				 ;sum 6
-(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
-			 seq))		;sum 10
-(stream-ref y 7)			;sum 136 output 136
-(display-stream z)			;sum 210
-;; outputs
+  sum)
+
+;; accum
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+;; sum 1
+
+(define y (stream-filter even? seq))
+;; sum 6
+
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) seq))
+;; sum 10
+
+(stream-ref y 7)
+;; 6
+;; 10
+;; 28
+;; 36
+;; 66
+;; 78
+;; 120
+;; 136
+
+;; 136
+;; sum 136
+
+(display-stream z)
 ;; 10
 ;; 15
 ;; 45
@@ -17,3 +38,10 @@
 ;; 120
 ;; 190
 ;; 210
+
+;; sum 210
+
+;; if implement procedure delay without memo-proc;
+;; the evaluation of stream seq will recalculate its elements;
+;; since we call procedure accum to calculate its element, 
+;; the global variable sum would keep changing.
