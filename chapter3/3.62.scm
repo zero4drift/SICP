b6242b021edb4598398ee660e88e05ec3fb5e90a
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-05 12:30:47 +0800

    A little rafactoring of ex 3.62.

--- chapter3/3.62.scm
+++ chapter3/3.62.scm
@@ -1,14 +1,12 @@
-(define (reciprocal s)
-  (cons-stream
-   (/ 1 (stream-car s))
-   (scale-stream (mul-series (stream-cdr s) (reciprocal s)) (- (/ 1 (stream-car s))))))
+;; (load "./ 3.61.scm")
 
 (define (div-series s1 s2)
   (if (= (stream-car s2) 0)
       (error "element 0 in the second stream")
-      (mul-series
-       s1
-       (reciprocal s2))))
+      (scale-stream (mul-series
+		     s1
+		     (calculate-x (scale-stream s2 (/ 1 (stream-car s2)))))
+		    (/ 1 (stream-car s2)))))
 
 (define tan-series
   (div-series sine-series cosine-series))
