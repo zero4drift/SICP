5facc32fd1b34146c5581e187cae1ce2a7ed8223
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-05 17:54:26 +0800

    A better implementation of ex 3.71.

--- chapter3/3.71.scm
+++ chapter3/3.71.scm
@@ -1,4 +1,4 @@
-(define (cube-pair pair)
+(define (cube-pair p)
   (+ (expt (car p) 3) (expt (cadr p) 3)))
 
 (define base-stream
@@ -7,26 +7,15 @@
    integers
    cube-pair))
 
-(define Ramanujan-pairs
-  (define (get-desired s1 s2)
-    (if (= (stream-car s1) 0)
-	(cons-stream
-	 (stream-car s2)
-	 (get-desired
-	  (stream-cdr s1)
-	  (stream-cdr s2)))
-	(get-desired
-	 (stream-cdr s1)
-	 (stream-cdr s2))))
-  (let ((rest-stream (stream-cdr base-stream))
-	(cube-streams (stream-map cube-pair base-stream)))
-    (let ((sub-stream (stream-map
-		       -
-		       cube-streams
-		       (stream-cdr cube-streams))))
-	 (get-desired
-	  sub-stream
-	  rest-stream))))
+(define (Ramanujan-pairs s1 s2)
+  (if (= (cube-pair (stream-car s1))
+	 (cube-pair (stream-car s2)))
+      (cons-stream (cube-pair (stream-car s1))
+		   (Ramanujan-pairs (stream-cdr s1)
+				    (stream-cdr s2)))
+      (Ramanujan-pairs (stream-cdr s1)
+		       (stream-cdr s2))))
 
 (define Ramanujan-streams
-  (stream-map cube-pair Ramanujan-pairs))
+  (Ramanujan-pairs base-stream
+		   (stream-cdr base-stream)))
