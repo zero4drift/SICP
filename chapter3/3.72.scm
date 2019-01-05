d4cfdaa158df819647dc6626b7d619793f38a8ea
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-05 18:14:31 +0800

    A pretty answer of flow sytle for ex 3.72.

--- chapter3/3.72.scm
+++ chapter3/3.72.scm
@@ -1,36 +1,25 @@
+(define (square-pair p)
+  (+ (square (car p)) (square (cadr p))))
+
 (define base-stream
   (weighted-pairs
    integers
    integers
-   (lambda (p) (+ (square (car p)) (square (cadr p))))))
+   square-pair))
 
-(define (square-pair p)
-  (+ (square (car p)) (square (cadr p))))
+(define (desired-pairs s1 s2 s3)
+  (if (= (square-pair (stream-car s1))
+	 (square-pair (stream-car s2))
+	 (square-pair (stream-car s3)))
+      (cons-stream (square-pair (stream-car s1))
+		   (desired-pairs (stream-cdr s1)
+				  (stream-cdr s2)
+				  (stream-cdr s3)))
+      (desired-pairs (stream-cdr s1)
+		     (stream-cdr s2)
+		     (stream-cdr s3))))
 
-(define desired-pairs
-  (define (get-desired s1 s2 s3)
-    (if (and (= (stream-car s1) 0) (= (stream-car s2) 0))
-	(cons-stream
-	 (stream-car s3)
-	 (get-desired
-	  (stream-cdr s1)
-	  (stream-cdr s2)
-	  (stream-cdr s3)))
-	(get-desired
-	 (stream-cdr s1)
-	 (stream-cdr s2)
-	 (stream-cdr s3))))
-  (let ((square-stream (stream-map square-pair base-stream))
-	(rest-stream (stream-cdr (stream-cdr base-stream))))
-    (let ((sub-stream1 (stream-map
-			-
-			square-stream
-			(stream-cdr square-stream)))
-	  (sub-stream2 (stream-map
-			-
-			(stream-cdr square-stream)
-			(stream-cdr (stream-cdr square-stream)))))
-      (get-desired sub-stream1 sub-stream2 rest-stream))))
-
-(define desired-numbers
-  (stream-map square-pair desired-pairs))
+(define desired-streams
+  (desired-pairs base-stream
+		 (stream-cdr base-stream)
+		 (stream-cdr (stream-cdr base-stream))))
