a3e7a179a333827bc0eccf0a44960eb9dea58abe
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-06 18:53:36 +0800

    A better implementation of ex 3.81.

--- chapter3/3.81.scm
+++ chapter3/3.81.scm
@@ -1,23 +1,14 @@
 ;; order-stream (('generate) ('reset 124) ...)
 
-(define (base-stream base)
-  (define rand-stream
+(define (random-numbers order-stream val)
+  (define (base-stream base)
     (cons-stream
      base
-     (stream-map rand-update rand-stream)))
-  rand-stream)
-
-(define (random-numbers order-stream rand-stream)
+     (random-numbers
+      (stream-cdr order-stream)
+      (rand-update base))))
   (cond ((eq? (car (stream-car order-stream)) 'generate)
-	 (cons-stream
-	  (stream-car (rand-stream))
-	  (random-numbers
-	   (stream-cdr order-stream)
-	   (stream-cdr rand-stream))))
+	 (base-stream val))
 	((eq? (car (stream-car order-stream)) 'reset)
-	 (random-numbers
-	  (stream-cdr order-stream)
-	  (base-stream (cdr (stream-car order-stream)))))))
-
-(define (random-numbers-order order-stream)
-  (random-numbers order-stream (base-stream random-init)))
+	 (base-stream (cadr (stream-car order-stream))))
+	(else "Invalid order" (stream-car order-stream))))
