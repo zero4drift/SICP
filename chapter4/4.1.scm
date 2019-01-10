9f3319275906f3733e89f5f3874b91cb3682fa29
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-10 14:46:16 +0800

    Refactoring of answer of ex 4.1.

--- chapter4/4.1.scm
+++ chapter4/4.1.scm
@@ -8,18 +8,18 @@
 
 ;; the one always computes the value from left to right
 
-(define (list-of-values exps env)
+(define (list-of-values-lr exps env)
   (if (no-operands? exps)
       '()
-      (let ((first (eval (first-operand exps) env))
-	    (rest-values (list-of-values (rest-operands exps) env)))
-	(cons first rest-values))))
+      (let ((first (eval (first-operand exps) env)))
+	(cons first
+	      (list-of-values-lr (rest-operands exps) env)))))
 
 ;; the one always computes the value from right to left
 
-(define (list-of-values exps env)
+(define (list-of-values-rl exps env)
   (if (no-operands? exps)
       '()
-      (let ((rest-values (list-of-values (rest-operands exps) env))
-	    (first (eval (first-operand exps) env)))
-	(cons first rest-values))))
+      (let ((rest-values (list-of-values-rl (rest-operands exps) env)))
+	(cons (eval (first-operand exps) env)
+	      rest-values))))
