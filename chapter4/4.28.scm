eab82085799aab930296c581372a44b95b60c645
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-12 21:20:54 +0800

    Add comment for ex 4.28.

--- chapter4/4.28.scm
+++ chapter4/4.28.scm
@@ -1,2 +1,5 @@
 ;; the operator maybe a thunk lambda object
-;; which must be evaluated by actual-value
+;; if not pass the actual-value result of operator to apply;
+;; within apply it would always fall to the third clause
+;; which reports error.
+;; so it must be evaluated by actual-value before passed to apply.
